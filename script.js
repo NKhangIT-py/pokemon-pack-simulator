dc32ec40f632812107e91e71cbc315774a036c81
NKhangIT-py <khangnguyenmm145@gmail.com>
2026-05-27 12:33:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,107 @@
 const API =
+"https://api.pokemontcg.io/v2/cards?pageSize=250";
+
+let allCards = [];
+
+let packCount = 50;
+
+let collection = [];
+
+async function loadCards(){
+
+    const res = await fetch(API);
+
+    const data = await res.json();
+
+    allCards = data.data;
+
+    console.log(allCards);
+}
+
+function updateUI(){
+
+    document.getElementById(
+        "packCount"
+    ).innerText = packCount;
+
+    document.getElementById(
+        "collectionCount"
+    ).innerText = collection.length;
+}
+
+function rarityClass(rarity){
+
+    if(!rarity)
+        return "common";
+
+    rarity = rarity.toLowerCase();
+
+    if(rarity.includes("secret"))
+        return "secret";
+
+    if(
+        rarity.includes("ultra") ||
+        rarity.includes("hyper") ||
+        rarity.includes("special")
+    )
+        return "ultra";
+
+    if(rarity.includes("rare"))
+        return "rare";
+
+    if(rarity.includes("uncommon"))
+        return "uncommon";
+
+    return "common";
+}
+
+function randomCard(){
+
+    return allCards[
+        Math.floor(
+            Math.random()*allCards.length
+        )
+    ];
+}
+
+function createCard(cardData){
+
+    const rarity = rarityClass(
+        cardData.rarity
+    );
+
+    const card =
+    document.createElement("div");
+
+    card.className =
+    `card ${rarity}`;
+
+    card.innerHTML = `
+
+        <img src="${cardData.images.large}">
+
+        <div class="info">
+            <h3>${cardData.name}</h3>
+            <p>${cardData.rarity || "Common"}</p>
+        </div>
+    `;
+
+    return {
+        element: card,
+        rarity
+    };
+}
+
+async function openPack(){
+
+    if(allCards.length===0)
+        return;
+
+    if(packCount<=0){
+
+        alert("Hết pack!");
+
+        return;
     }
 
     packCount--;
